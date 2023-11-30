04881d312e21a87a2327dd8add1f5820c9d31a8d
Isak <isak@maatta.se>
2023-11-30 15:31:49 +0100

    Tagga aoc!

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,6 +12,8 @@ fun main() {
     check(part1(testInput) == 1)
 
     val input = readInput("Day01")
+    part1(testInput).println()
     part1(input).println()
+    part2(testInput).println()
     part2(input).println()
 }
